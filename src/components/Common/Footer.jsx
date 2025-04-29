4553f5724d2c24c526cf237923ffced4d414e492
Deepak Sharma <123837272+Deepaksharmadk@users.noreply.github.com>
2025-04-29 07:46:53 +0530

    Update Footer.jsx

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="bg-richblack-800">
             <div className="mx-auto flex w-11/12 max-w-maxContent flex-row items-center justify-between p-7 text-sm text-richblack-400">
                 <div className="w-full text-center text-justify text-lg">
-                    <strong> Copyright © 2024 E-LMS | </strong> Made with ❤️
+                    <strong> Copyright © 2025 E-LMS | </strong> Made with ❤️
                     Deepak, Bipin, Amarjeet, Vicky, Shivam |
                 </div>
             </div>
